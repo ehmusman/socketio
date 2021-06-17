22d50c950336fca94193f95ee0c6d678d30aea3a
ehmusman <ehmusman@gmail.com>
2021-06-17 18:03:29 +0500

    router created

--- server/index.js
+++ server/index.js
@@ -4,11 +4,19 @@ const http = require("http")
 const dotenv = require("dotenv")
 dotenv.config()
 const app = express()
-
+const router = require("./router")
 const server = http.createServer(app)
 
 const io = socketio(server)
+io.on("connection", socket => {
+    console.log("We have a new Connection !!!")
 
+    socket.on("disconnect", () => {
+        console.log("User Had Left!!!")
+    })
+})
+
+app.use(router)
 
 const PORT = process.env.PORT || 6000
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
