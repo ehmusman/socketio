4758b6a22da588f523a1232a18fe2f7480da9752
ehmusman <ehmusman@gmail.com>
2021-06-18 10:33:05 +0500

    join component is styled

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,13 @@ import Chat from "./components/Chat";
 import Join from "./components/Join"
 function App() {
   return (
-    <div className="container">
-      <Router>
-        <Route exact path="/" component={Join} />
-        <Route exact path="/chat" component={Chat} />
-      </Router>
-
+    <div className="bg-dark text-light" style={{ minHeight: "100vh" }}>
+      <div className="container">
+        <Router>
+          <Route exact path="/" component={Join} />
+          <Route exact path="/chat" component={Chat} />
+        </Router>
+      </div>
     </div>
   );
 }
