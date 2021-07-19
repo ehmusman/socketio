c473207ec4c047e9353b1898a8b13bf825e63f1b
ehmusman <ehmusman@gmail.com>
2021-07-19 17:48:23 +0500

    chat app

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
-import { BrowserRouter as Router, Route } from "react-router-dom"
-import Chat from "./components/Chat";
-import Join from "./components/Join"
-function App() {
+import React from 'react';
+
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="bg-dark text-light" style={{ minHeight: "100vh" }}>
-      <div className="container">
-        <Router>
-          <Route exact path="/" component={Join} />
-          <Route exact path="/chat" component={Chat} />
-        </Router>
-      </div>
-    </div>
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,49 +1,53 @@
-const express = require("express")
-const http = require("http")
-const dotenv = require("dotenv")
-dotenv.config()
-const socketio = require("socket.io")
-const app = express()
-const router = require("./router")
-const server = http.createServer(app)
-const cors = require("cors")
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
 
-const { getUser, getUserInRoom, removeUser, addUser } = require("./users")
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
-app.use(cors())
+const router = require('./router');
 
-const io = socketio(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(cors());
+app.use(router);
+
+io.on('connection', (socket) => {
+  console.log(socket.userr)
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    console.log(message)
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
     }
+  })
 });
-io.on("connection", socket => {
 
-    socket.on("join", ({ name, room }, callback) => {
-        console.log(name, room)
-        const { error, user } = addUser({ id: socket.id, name, room })
-        if (error) return callback(error)
-        console.log("after user seraching")
-        socket.emit("message", { user: "admin", text: `${user.name}, Welcom to the room ${user.room}` })
-        console.log("message of joining")
-        socket.broadcast.to(user.room).emit("message", { user: "admin", text: `${user.name}, has Joined!` });
-        console.log("Message to every one")
-
-        socket.join(user.room)
-        callback()
-    })
-    socket.on("sendMessage", (message, callback) => {
-        const user = getUser(socket.id)
-        io.to(user.room).emit("message", { user: user.name, text: message })
-        callback()
-    })
-    socket.on("disconnect", () => {
-
-    })
-})
-
-app.use(router)
-
-const PORT = 6000
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
